1e3d8d7bed7cd6cfc1d3dc87d01e905969cf9727
Csandori <csandori@gmail.com>
2020-12-03 13:30:45 +0100

    Csak Tominak

--- 1.js
+++ 1.js
@@ -200,12 +200,25 @@ let data = [
   1050,
   1293,
 ];
-for (let i = 0; i < data.length; i++) {
-  for (let j = i + 1; j < data.length; j++) {
-    for (let k = j + 1; k < data.length; k++) {
-      if (data[i] + data[j] + data[k] === 2020) {
-        console.log(data[j] * data[i] * data[k]);
+function one() {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[i] + data[j] === 2020) {
+        console.log(data[j] * data[i]);
       }
     }
   }
 }
+function two() {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      for (let k = j + 1; k < data.length; k++) {
+        if (data[i] + data[j] + data[k] === 2020) {
+          console.log(data[j] * data[i] * data[k]);
+        }
+      }
+    }
+  }
+}
+one();
+two();
