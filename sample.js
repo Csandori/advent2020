d049d46f365be6d8b4c9c3a68c515ef831f0af7a
Csandori <csandori@gmail.com>
2020-12-08 08:53:04 +0100

    after 8. day

---
+++ sample.js
@@ -0,0 +1,17 @@
+let input = ``
+
+function one(){
+    let lines=input.split(/[\n\r]/gm)
+    let object = {}
+    lines.forEach(line=>{
+        [...line].forEach(char=>{
+
+        })
+    })
+
+    let count=0
+    console.log(count)
+}
+
+one()
+two()
